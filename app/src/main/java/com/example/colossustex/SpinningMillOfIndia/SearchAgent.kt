bec45db89ae24b6c215ca3dd2a3d191e013dd6cd
Abhishek3970 <52279793+Abhishek3970@users.noreply.github.com>
2020-03-07 19:22:04 +0530

    added navigation to search agent section

--- app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SearchAgent.kt
+++ app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SearchAgent.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
 
 import com.example.colossustex.R
 
@@ -18,7 +20,23 @@ class SearchAgent : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_agent, container, false)
+        var lay = inflater.inflate(R.layout.fragment_search_agent, container, false)
+        val toolbar = lay.findViewById<Toolbar>(R.id.toolbar_spinning_mills_in_india_search_agent)
+        toolbar.setNavigationOnClickListener{
+            it.findNavController().navigate(SearchAgentDirections.actionSearchAgentToSpinningMillOfIndia())
+        }
+        toolbar.inflateMenu(R.menu.menu_spinning_mills_of_india)
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.home_page -> {
+                    toolbar.findNavController()
+                        .navigate(SearchAgentDirections.actionSearchAgentToHomePage())
+                }
+            }
+            true
+        }
+
+        return lay
     }
 
 }
