9971306cb635decf367a9c79c4260d214c33e573
Abhishek3970 <52279793+Abhishek3970@users.noreply.github.com>
2020-03-07 16:42:20 +0530

    removed unnessary comments

--- app/src/main/java/com/example/colossustex/homePage/HomePage.kt
+++ app/src/main/java/com/example/colossustex/homePage/HomePage.kt
@@ -30,10 +30,7 @@ class HomePage : Fragment() {
 
     private lateinit var viewModel: HomePageViewModel
 
-    //Just adding some comments to check VCS
-    // SAME AS ABOVE
-    //test
-    //test1
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
