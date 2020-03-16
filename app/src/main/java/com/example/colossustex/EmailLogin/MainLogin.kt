e5d707741d1d54e6369d153116f6c394f8aab11e
Anubhav <transformer12900@gmail.com>
2020-03-17 01:21:02 +0530

    Made the login for the app along with register

--- app/src/main/java/com/example/colossustex/EmailLogin/MainLogin.kt
+++ app/src/main/java/com/example/colossustex/EmailLogin/MainLogin.kt
@@ -1,13 +1,86 @@
 package com.example.colossustex.EmailLogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.colossustex.MainActivity
 import com.example.colossustex.R
+import com.example.colossustex.databinding.ActivityMainLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainLogin : AppCompatActivity() {
-
+    private lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityMainLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_login)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main_login)
+        auth = FirebaseAuth.getInstance()
+        binding.textRegister.setOnClickListener {
+            val intent= Intent(this,MainRegister::class.java)
+            startActivity(intent)
+        }
+        binding.loginBtn.setOnClickListener {
+            dologin()
+        }
+    }
+
+    private fun dologin() {
+        if(binding.emaillogin.text.toString()==""){
+            binding.emaillogin.error="Enter Email"
+            binding.emaillogin.requestFocus()
+            return
+        }
+        if(binding.passlogin.text.toString()==""||(binding.passlogin.text.toString().length)<6){
+            binding.passlogin.error="Enter min 6 Chars"
+            binding.passlogin.requestFocus()
+            return
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(binding.emaillogin.text.toString()).matches()){
+            binding.emaillogin.error="Enter Proper Email"
+            binding.emaillogin.requestFocus()
+            return
+        }
+        auth.signInWithEmailAndPassword(binding.emaillogin.text.toString(), binding.passlogin.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser!=null){
+            if(currentUser.isEmailVerified) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if(currentUser!=null){
+            if(currentUser.isEmailVerified) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+           }else{
+               Toast.makeText(this,"Verify your email",Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(baseContext, "Login Failed.",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/colossustex/EmailLogin/MainRegister.kt
+++ app/src/main/java/com/example/colossustex/EmailLogin/MainRegister.kt
@@ -1,13 +1,81 @@
 package com.example.colossustex.EmailLogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.colossustex.R
+import com.example.colossustex.databinding.ActivityMainRegisterBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.texturised_fragment1.*
 
 class MainRegister : AppCompatActivity() {
-
+    lateinit var auth: FirebaseAuth
+    lateinit var binding: ActivityMainRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_register)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main_register)
+        auth=FirebaseAuth.getInstance()
+        binding.resgisterBtn.setOnClickListener {
+            signup()
+        }
+        binding.loginNowText.setOnClickListener {
+            val intent=Intent(this,MainLogin::class.java)
+            .apply {  flags=Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK }
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_left,R.anim.still)
+
+        }
+
+    }
+
+    private fun signup() {
+        if(binding.nameEt.text.toString()==""){
+            binding.nameEt.error="Enter Name"
+            binding.nameEt.requestFocus()
+            return
+        }
+        if(binding.emailEt.text.toString()==""){
+            binding.emailEt.error="Enter Email"
+            binding.emailEt.requestFocus()
+            return
+        }
+        if(binding.passEt.text.toString()==""||(binding.passEt.text.toString().length)<6){
+            binding.passEt.error="Enter min 6 Chars"
+            binding.passEt.requestFocus()
+            return
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(binding.emailEt.text.toString()).matches()){
+            binding.emailEt.error="Enter Proper Email"
+            binding.emailEt.requestFocus()
+            return
+        }
+        auth.createUserWithEmailAndPassword(binding.emailEt.text.toString(), binding.passEt.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user=auth.currentUser
+                    user?.sendEmailVerification()
+                        ?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Snackbar.make(binding.registerLayout,"Email Sent",Snackbar.LENGTH_SHORT).show()
+                                startActivity(Intent(this,MainLogin::class.java))
+                                finish()
+                            }else{
+                                Toast.makeText(this,"Please check your network",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                } else {
+                    Toast.makeText(baseContext, "Sign Up Failed.Try After some time.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
 }
