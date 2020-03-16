f25529bb8a87fee0b94ccebf9fafde7aa0852ede
Anubhav <transformer12900@gmail.com>
2020-03-16 22:37:25 +0530

    Merged master

---
+++ app/src/main/java/com/example/colossustex/EmailLogin/MainRegister.kt
@@ -0,0 +1,13 @@
+package com.example.colossustex.EmailLogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.colossustex.R
+
+class MainRegister : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_register)
+    }
+}
