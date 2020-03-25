16ff19bfda1d4445093af3ae62a3dc0ad2214d89
Abhishek3970 <52279793+Abhishek3970@users.noreply.github.com>
2020-03-25 11:48:54 +0530

    added icons to navigation

--- app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SpinningMillOfIndia.kt
+++ app/src/main/java/com/example/colossustex/SpinningMillOfIndia/SpinningMillOfIndia.kt
@@ -47,15 +47,15 @@ class SpinningMillOfIndia : Fragment() {
         var lay = inflater.inflate(R.layout.fragment_spinning_mill_of_india, container, false)
         val toolbar =
             lay.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_spinning_mills_in_india)
-        val cotton = lay.findViewById<TextView>(R.id.textView_cotton)
+        val cotton = lay.findViewById<ImageView>(R.id.textView_cotton)
         cotton.setOnClickListener {
             it.findNavController().navigate(R.id.action_spinningMillOfIndia_to_cottonFragment)
         }
-        val synthetic = lay.findViewById<TextView>(R.id.textView_Synthetic)
+        val synthetic = lay.findViewById<ImageView>(R.id.textView_Synthetic)
         synthetic.setOnClickListener {
             it.findNavController().navigate(R.id.action_spinningMillOfIndia_to_syntheticFragment)
         }
-        val viscose = lay.findViewById<TextView>(R.id.textView_viscose)
+        val viscose = lay.findViewById<ImageView>(R.id.textView_viscose)
         viscose.setOnClickListener {
             startActivity(
                 Intent(
@@ -64,11 +64,11 @@ class SpinningMillOfIndia : Fragment() {
                 )
             )
         }
-        val texturised = lay.findViewById<TextView>(R.id.textView_texturised)
+        val texturised = lay.findViewById<ImageView>(R.id.textView_texturised)
         texturised.setOnClickListener {
             startActivity(Intent(context, TexturisedActivity::class.java))
         }
-        val fancy = lay.findViewById<TextView>(R.id.textView_fancy)
+        val fancy = lay.findViewById<ImageView>(R.id.textView_fancy)
         fancy.setOnClickListener {
             startActivity(Intent(context, FancyActivity::class.java))
         }
