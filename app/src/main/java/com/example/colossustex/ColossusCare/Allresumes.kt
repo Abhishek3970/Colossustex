0285e6e5aaacc114b81363c4fb37ec66c4366fb5
Anubhav <transformer12900@gmail.com>
2020-04-13 18:40:33 +0530

    ColossusCare

--- app/src/main/java/com/example/colossustex/ColossusCare/Allresumes.kt
+++ app/src/main/java/com/example/colossustex/ColossusCare/Allresumes.kt
@@ -3,11 +3,11 @@ package com.example.colossustex.ColossusCare
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.colossustex.R
-import com.example.colossustex.databinding.AllResumesBinding
 
 class Allresumes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.allresume_activity)
     }
+
 }
--- app/src/main/java/com/example/colossustex/ColossusCare/AllresumesData.kt
+++ app/src/main/java/com/example/colossustex/ColossusCare/AllresumesData.kt
@@ -6,7 +6,7 @@ data class AllresumesData(
     val location: String = "",
     val ctc: String = "",
     val time: String = "",
-    val emp_details: String = "",
+    val emp_details: String ="",
     val academic_details: String = "",
     val accomplishments: String = ""
 )
